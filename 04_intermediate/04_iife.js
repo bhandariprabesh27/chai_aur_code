1ecba318cb713d163bd6ccbf66586ce26371d256
Prabesh Bhandari <130889992+bhandariprabesh27@users.noreply.github.com>
2024-05-29 16:59:50 +0000

    last commit for iife

--- 04_intermediate/04_iife.js
+++ 04_intermediate/04_iife.js
@@ -5,4 +5,11 @@
     console.log("DB CONNECTED!!!")
 })();
 
+
 (()=>{console.log("DB CONNECTED 2!!!")})(); //arrow function ma ni kaam garxa
+
+
+
+(function coffee(cName){
+    console.log(`The coffee you have entered is ${cName}.`)
+})("Premium Beans");      // yeha j value pass garyo tyo function ko parameter ma janxa
