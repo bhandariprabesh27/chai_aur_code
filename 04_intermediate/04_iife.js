649d54c842b479c7fc10fd02ee6a9b88bee33796
Prabesh Bhandari <130889992+bhandariprabesh27@users.noreply.github.com>
2024-05-29 16:45:18 +0000

    iife

---
+++ 04_intermediate/04_iife.js
@@ -0,0 +1,8 @@
+//iife = immediately invoked function expression
+//syntax:   (function fname(){expression;})();
+
+(function chai(){
+    console.log("DB CONNECTED!!!")
+})();
+
+(()=>{console.log("DB CONNECTED 2!!!")})(); //arrow function ma ni kaam garxa
