ad554d4e20578774b70c2a386fbc4a8ead770395
Prabesh Bhandari <130889992+bhandariprabesh27@users.noreply.github.com>
2024-06-03 18:22:28 +0000

    looop 2

---
+++ 06_iterations/02_two.js
@@ -0,0 +1,27 @@
+// let index = 0;
+// while(index<=10){
+//     console.log(`value of index is ${index}`);
+//     index = index + 2;
+// }
+
+
+let myArr = ["Kushal","Sandy","Aasif"];
+let index = 0;
+while(index < myArr.length){
+    console.log(`value of array is ${myArr[index]}`);
+    index = index + 1;
+}
+
+
+// let score = 0;
+// do{
+// console.log(`score is ${score}`);
+// score++;
+// }while(score<=10)   //paila loop vitra ko chalxa ani last ma condition check garxa
+
+
+let marks = 11;
+do{
+    console.log(`Marks is ${marks}`); //yo line print hunxa ani balla condition tala check hunxa so output will be 11
+    marks++;
+}while(marks<=10) 
